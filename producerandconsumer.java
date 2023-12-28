0cd7d361a063e021a4f72eb16e9c20c0957a5ffb
Madhur Verma <103921455+cisojoker@users.noreply.github.com>
2023-12-28 18:26:42 +0530

    Update producerandconsumer.java

--- producerandconsumer.java
+++ producerandconsumer.java
@@ -1,55 +1,45 @@
+import java.util.LinkedList;
+import java.util.Queue;
 
-import java.util.*;
-public class Main{
-    public static void main(String[] args){
-        Object key=new Object();
-        Queue<Integer>queue=new LinkedList<Integer>();
-        int size=8;
-        Thread producer=new Thread(new Runnable(){
-            @Override
-            public void run(){
-                int count=0;
-               while(true){ 
-                   synchronized(key){
-                    
-                    try{
-                        while(queue.size()==size)
-                        key.wait();
-                        
-                       queue.offer(count++);
-                       key.notifyAll();
-                       
-                       Thread.sleep(1000);
-                        System.out.println("producer: "+queue.size());
-                    }catch(InterruptedException e){
+public class Main {
+    public static void main(String[] args) {
+        Object key = new Object();
+        Queue<Integer> queue = new LinkedList<>();
+        int size = 8;
+
+        Thread producer = new Thread(() -> {
+            int count = 0;
+            while (true) {
+                synchronized (key) {
+                    try {
+                        while (queue.size() == size) key.wait();
+                        queue.offer(count++);
+                        key.notifyAll();
+                        Thread.sleep(1000);
+                        System.out.println("producer: " + queue.size());
+                    } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
                 }
-               }
             }
         });
-         Thread consumer=new Thread(new Runnable(){
-            @Override
-            public void run(){
-                while(true){
-                  synchronized(key){
-                   
-                    try{
-                         while(queue.size()==0)
-                        key.wait();
-                        
+
+        Thread consumer = new Thread(() -> {
+            while (true) {
+                synchronized (key) {
+                    try {
+                        while (queue.size() == 0) key.wait();
                         queue.poll();
                         key.notifyAll();
-                        
                         Thread.sleep(1000);
-                        System.out.println("consumer: "+queue.size());
-                    }catch(InterruptedException e){
+                        System.out.println("consumer: " + queue.size());
+                    } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
-                }    
-              }
+                }
             }
         });
+
         producer.start();
         consumer.start();
     }
