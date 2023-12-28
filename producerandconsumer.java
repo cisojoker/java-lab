df473ce7fce4d06e63ceb777d4b3d2561707bf5f
Madhur Verma <103921455+cisojoker@users.noreply.github.com>
2023-12-28 20:36:20 +0530

    Update producerandconsumer.java

--- producerandconsumer.java
+++ producerandconsumer.java
@@ -1,43 +1,41 @@
 import java.util.*;
-
-public class Main {
-    public static void main(String[] args) {
-        Object key = new Object();
-        Queue<Integer> queue = new LinkedList<>();
-        int size =2;
-        int maxIterations = 4; 
-
-        Thread producer = new Thread(() -> {
-            int count = 0;
-            int iterations = 0;
-            while (iterations < maxIterations) {
-                synchronized (key) {
-                    try {
-                        while (queue.size() == size) key.wait();
+class Main{
+    public static void main(String[] args){
+        Object key=new Object();
+        Queue<Integer> queue=new LinkedList<>();
+        
+        int size=2;
+        int maxiterations=4;
+        
+        Thread producer=new Thread(()->{
+            int count=0;int iterations=0;
+            while(iterations<maxiterations){
+                synchronized(key){
+                    try{
+                        while(queue.size()==size) key.wait();
                         queue.offer(count++);
                         key.notifyAll();
                         Thread.sleep(1000);
-                        System.out.println("producer: " + queue.size());
+                        System.out.println("producer: "+queue.size());
                         iterations++;
-                    } catch (InterruptedException e) {
+                    }catch(InterruptedException e){
                         e.printStackTrace();
                     }
                 }
             }
         });
-
-        Thread consumer = new Thread(() -> {
-            int iterations = 0;
-            while (iterations < maxIterations) {
-                synchronized (key) {
-                    try {
-                        while (queue.size() == 0) key.wait();
+        Thread consumer=new Thread(()->{
+           int iterations=0;
+            while(iterations<maxiterations){
+                synchronized(key){
+                    try{
+                        while(queue.size()==0) key.wait();
                         queue.poll();
                         key.notifyAll();
                         Thread.sleep(1000);
-                        System.out.println("consumer: " + queue.size());
+                        System.out.println("consumer: "+(queue.size()+1));
                         iterations++;
-                    } catch (InterruptedException e) {
+                    }catch(InterruptedException e){
                         e.printStackTrace();
                     }
                 }
@@ -45,11 +43,10 @@ public class Main {
         });
         producer.start();
         consumer.start();
-
         try {
             producer.join();
             consumer.join();
-        } catch (InterruptedException e) {
+        }catch(InterruptedException e){
             e.printStackTrace();
         }
     }
