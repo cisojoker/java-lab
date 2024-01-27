045816e742f1f2590a0d4b1e7f93596cedacadee
Madhur Verma <103921455+cisojoker@users.noreply.github.com>
2024-01-27 18:51:06 +0530

    Update and rename question3.java to semendpg3.java

--- semendpg3.java
+++ semendpg3.java
@@ -3,21 +3,25 @@ class complex{
     private double real;
     private double imaginary;
     
-    public complex(double r,double im){
+    complex(double r,double i){
         real=r;
-        imaginary=im;
+        imaginary=i;
     }
+    
     public complex add(complex x){
-        double t1=this.real+x.real;
-        double t=this.imaginary+x.imaginary;
-        return new complex(t1,t);
+        double re=this.real+x.real;
+        double im=this.imaginary+x.imaginary;
+        return new complex(re,im);
     }
       public complex sub(complex x){
-        double t1=this.real-x.real;
-        double t=this.imaginary-x.imaginary;
-        return new complex(t1,t);
+        double re=this.real-x.real;
+        double im=this.imaginary-x.imaginary;
+        return new complex(re,im);
     }
-    public void increment(){
+    public boolean compare(complex x){
+        return this.real==x.real && this.imaginary==x.imaginary;
+    }
+     public void increment(){
         this.real++;
         this.imaginary++;
     }
@@ -25,18 +29,13 @@ class complex{
         this.real--;
         this.imaginary--;
     }
-    
-    public boolean isEqual(complex x){
-        return this.real==x.real && this.imaginary==x.imaginary;
-    }
     public void display(){
         System.out.println("complex number is");
-        System.out.println(this.real+"+i"+this.imaginary);
+        System.out.println(this.real+"+"+this.imaginary+"i");
         System.out.println();
     }
 }
-    
-public class Encapsulation{
+public class Main{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter the real and imaginary part of first Complex Number: ");
@@ -46,28 +45,29 @@ public class Encapsulation{
         double r2 = sc.nextDouble();
         double i2 = sc.nextDouble();
         
-        complex c1=new complex(r1,i1);
-        complex c2=new complex(r2,i2);
-          
+        complex a=new complex(r1,i1);
+        complex b=new complex(r2,i2);
         System.out.println("The two complex numbers are: ");
-        c1.display();
-        c2.display();
+        a.display();
+        b.display();
         System.out.println("The addition: ");
-        complex c3=c1.add(c2);
-        c3.display();
+        complex res1=a.add(b);
+        res1.display();
         System.out.println("The subtraction: ");
-        complex c4=c1.sub(c2);
-        c4.display();
-        System.out.println("The equality: ");
-        boolean flag=c1.isEqual(c2);
-        if(flag==true)System.out.println("equal");
-        else System.out.println("not equal");
+        complex res2=a.sub(b);
+        res2.display();
+        System.out.println("checking for equality: ");
+        if(a.compare(b)==true){
+            System.out.println("both complex number are equal");
+        }else{
+            System.out.println("not equal");
+        }
+        
         System.out.println("The Incrementation first number is: ");
-        c1.increment();
-        c1.display();
+        a.increment();
+        a.display();
        System.out.println("The Decrementation second number is: ");
-        c2.decrement();
-        c2.display();
+        b.decrement();
+        b.display();
     }
 }
-      
