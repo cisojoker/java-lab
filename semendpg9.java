1b6666e5af4ffea1b797b2086c42eac7b02e0a58
Madhur Verma <103921455+cisojoker@users.noreply.github.com>
2024-01-28 11:43:04 +0530

    Update semendpg9.java

--- semendpg9.java
+++ semendpg9.java
@@ -4,25 +4,24 @@ interface compute{
 }
 class gbconvert implements compute{
     public double convert(double val){
-        return (val/1000000);
+        return (val*1000000);
     }
 }
 class euroconvert implements compute{
     public double convert(double val){
-        return val*90.00;
-    }
+            return val*90;
+        }
 }
-class Main{
+public class Main{
     public static void main(String[] args){
         compute c1=new gbconvert();
         compute c2=new euroconvert();
         Scanner sc=new Scanner(System.in);
-        System.out.println("enter the value you want to conver to gb");
+        System.out.println("enter the values in gb");
         double a=sc.nextDouble();
         double b=c1.convert(a);
-        System.out.println(a+" value in gb is "+b);
-        
-       System.out.println("enter the value you want to conver to euro");
+        System.out.println(a+" value in bytes is "+b);
+        System.out.println("enter the value you want to conver to euro");
         double aa=sc.nextDouble();
         double bb=c2.convert(aa);
         System.out.println(aa+" value in euro is "+bb);
