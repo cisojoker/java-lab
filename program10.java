0c381397433eaf39c2eefc08bbfaa55a3415da29
Madhur Verma <103921455+cisojoker@users.noreply.github.com>
2024-01-28 13:17:42 +0530

    semendpg10.java

--- program10.java
+++ program10.java
@@ -1,45 +1,61 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.Scanner;
 
-public class App {
-
+public class jdbc {
     public static void main(String[] args) {
         // Replace these with your database details
-        String jdbcUrl = "jdbc:mysql://localhost:3306/student_details";
+        Scanner sc = new Scanner(System.in);
+        String jdbcUrl = "jdbc:mysql://localhost:3306/students";
         String username = "root";
-        String password = "root";
-
+        String password = "";
         try {
             // Establishing the database connection
             Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
-
-            // Retrieving student details
-            String query = "SELECT * FROM students";
-            PreparedStatement preparedStatement = connection.prepareStatement(query);
-            ResultSet resultSet = preparedStatement.executeQuery();
-
-            // Displaying student details
-            while (resultSet.next()) {
-                String name = resultSet.getString("name");
-                String usn = resultSet.getString("usn");
-                int semester = resultSet.getInt("semester");
-                int age = resultSet.getInt("age");
-                String course = resultSet.getString("course");
-                String dob = resultSet.getString("dob");
-                String address = resultSet.getString("address");
-
-                System.out.println("Name: " + name + ", USN: " + usn + ", Semester: " + semester +
-                        ", Age: " + age + ", Course: " + course + ", DOB: " + dob + ", Address: " + address);
+            // Driver Program
+            while (true) {
+                System.out.println(
+        "1. Add student details.\n2. Display student details\n3. Delete student details\n4. Update Student Details\n");
+                System.out.println("Please choose an option: ");
+                int choice = sc.nextInt();
+                sc.nextLine();
+                if (choice == 1) {
+                    String queryInsert = "INSERT INTO students(Name,USN,Age,YOB,Marks) VALUES ('pp','40',15,2005,45);";
+                    PreparedStatement preparedStatement = connection.prepareStatement(queryInsert);
+                    preparedStatement.executeUpdate();
+                } else if (choice == 2) {
+                    // Retrieving student details
+                    String query = "SELECT * FROM students";
+                    PreparedStatement preparedStatement = connection.prepareStatement(query);
+                    ResultSet resultSet = preparedStatement.executeQuery();
+                    // Displaying student details
+                    while (resultSet.next()) {
+                        String name = resultSet.getString("Name");
+                        String usn = resultSet.getString("USN");
+                        int age = resultSet.getInt("Age");
+                        int yob = resultSet.getInt("YOB");
+                        int marks = resultSet.getInt("Marks");
+                        System.out.println("Name: " + name + ", USN: " +
+                                usn + ", Age: " + age + ", YearofBirth: " + yob
+                                + ", Marks: " + marks);
+                    }
+                } else if (choice == 3) {
+                    System.out.println("Please enter the USN of the student to be deleted: ");
+                    String usndel = sc.nextLine();
+                    String queryDel = "DELETE FROM students WHERE USN='" + usndel + "'";
+                    PreparedStatement preparedStatement = connection.prepareStatement(queryDel);
+                    preparedStatement.executeUpdate();
+                } else if (choice == 4) {
+                    System.out.println("Please enter the USN of the student to be updated: ");
+                    String usnUpd = sc.nextLine();
+                    System.out.println("Please enter the new name: ");
+                    String newName = sc.nextLine();
+                    String updQuery = "UPDATE students SET name=?WHERE USN=?";
+                    PreparedStatement pstmt = connection.prepareStatement(updQuery);
+                    pstmt.setString(1, newName);
+                    pstmt.setString(2, usnUpd);
+                    pstmt.executeUpdate();
+                }
             }
-
-            // Closing resources
-            resultSet.close();
-            preparedStatement.close();
-            connection.close();
-
         } catch (SQLException e) {
             e.printStackTrace();
         }
